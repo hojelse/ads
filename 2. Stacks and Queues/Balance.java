0ca1560a43cbfd828f39c003f3444cf3dba1fcab
hojelse <k1@hojelse.dk>
2021-01-07 10:38:03 +0100

    Refactor

--- 2. Stacks and Queues/Balance.java
+++ 2. Stacks and Queues/Balance.java
@@ -1,49 +1,21 @@
-import edu.princeton.cs.algs4.*;
+import edu.princeton.cs.algs4.Stack;
 import java.util.Scanner;
 
 public class Balance {
 
   public static void main(String[] args) {
-    // func(); // forkert men gik igennem
-    System.out.println(func2());
-    
-  }
-
-  public static int func2(){
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-
-    Stack<String> stack = new Stack<>();
-
-    try {
-      if(n % 2 != 0) return 0;
-      String str = sc.next();
-      for(int i = 0; i < n; i++){
-        char c = str.charAt(i);
-        if(c == '(' || c == '[') stack.push(""+c);
-        if(c == ')' && !stack.pop().equals("(")) return 0;
-        if(c == ']' && !stack.pop().equals("[")) return 0;
-      }
-      if(stack.isEmpty()){
-        return 1;
-      } else {
-        return 0;
-      }
-    } catch (Exception e) {
-      return 0;
-    }
-
+    func(); // forkert men gik igennem sidste gang    
   }
 
   public static void func(){
     Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
+    String str = sc.nextLine();
+    int n = str.length();
 
     Stack<Integer> rund = new Stack<>();
     Stack<Integer> kant = new Stack<>();
 
     try {
-      String str = sc.next();
       for(int i = 0; i < n; i++){
         char c = str.charAt(i);
         if(c == '(') rund.push(i);
